1473895a6431f77eeb891a366b4f11da16b7fb9b
misturfixit <japullins420@gmail.com>
2018-01-16 15:56:21 -0500

    got navbar mostly built, erbs for about and contact pages... linked out to Thriva survey

--- app.rb
+++ app.rb
@@ -2,5 +2,17 @@ require 'sinatra'
 enable :sessions
 
 get "/" do
-    erb :homePage
+    erb :home
 end
+
+get "/contact" do
+    erb :contact
+end
+
+get "/about" do
+    erb :about
+end
+
+get "/survey" do
+    erb :survey
+end    
